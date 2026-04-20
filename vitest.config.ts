ed0856658053238352f28ef2b87c0793a5d3e6af
woxQAQ <woxqaq@gmail.com>
2026-04-20 16:04:49 +0800

    chore: vitest report only failure cases

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "vitest/config";
+import FailureOnlyReporter from "./scripts/vitest-failure-only-reporter";
 
 export default defineConfig({
   test: {
     include: ["packages/bridge/**/*.test.ts", "packages/web/src/**/*.test.ts"],
+    reporters: ["default"]
+    // reporters: [new FailureOnlyReporter(),],
   },
 });
